533dc31a61f4d0f93c3f1178b6a1f1de57136ba8
Ben Coleman <benc.uk@gmail.com>
2020-06-04 16:02:35 +0100

    testing

--- api/testGet.cs
+++ api/testGet.cs
@@ -13,10 +13,20 @@ namespace api
     public static class testGet
     {
         [FunctionName("testGet")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "foo")] HttpRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-            string responseMessage = "HELLO I AM TASKR";
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
             return new OkObjectResult(responseMessage);
         }
     }
