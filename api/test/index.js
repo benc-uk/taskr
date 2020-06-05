0fb1ca2149c4ba6fd9fe62bd3973bb87ef4038fb
Ben Coleman <benc.uk@gmail.com>
2020-06-05 12:27:53 +0100

    header test

--- api/test/index.js
+++ api/test/index.js
@@ -1,8 +1,15 @@
 module.exports = async function (context, req) {
   context.log(JSON.stringify(context.bindings.req.headers));
 
+  const header = req.headers["x-ms-client-principal"];
+  const encoded = Buffer.from(header, "base64");
+  const decoded = encoded.toString("ascii");
+
   context.res = {
     status: 200,
-    body: context.bindings.req.headers
+    body: {
+      rawHeaders: context.bindings.req.headers,
+      clientPrincipal: JSON.parse(decoded)
+    }
   };
 };
