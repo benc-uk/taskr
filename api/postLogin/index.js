5990ca9178c765e67b4057a0c851490b85f06353
Ben Coleman <benc.uk@gmail.com>
2020-06-07 14:20:48 +0100

    postLogin

---
+++ api/postLogin/index.js
@@ -0,0 +1,41 @@
+const cosmos = require('../lib/cosmos.js')
+
+module.exports = async function (context, req) {
+  let errorStatus = 500
+  try {
+    let user = {}
+    // Get the owner from the special client-principal header
+    if (req.headers['x-ms-client-principal']) {
+      const encoded = Buffer.from(req.headers['x-ms-client-principal'], 'base64')
+      const principal = encoded.toString('ascii')
+      user.name = principal.userDetails
+      user.id = principal.userId
+    } else {
+      context.res = {
+        headers: { 'content-type': 'application/json' },
+        status: 401,
+        body: { message: 'Not authorized', error: true }
+      }
+      return
+    }
+
+    // Get Cosmos DB container
+    const users = await cosmos.users()
+
+    // Create/update user in Cosmos
+    const newItem = await users.items.upsert(user)
+
+    context.res = {
+      headers: { 'content-type': 'application/json' },
+      status: newItem.statusCode,
+      body: newItem.resource
+    }
+  } catch (err) {
+    let message = typeof err != 'string' ? (err.message ? err.message : err) : err
+    context.res = {
+      headers: { 'content-type': 'application/json' },
+      status: errorStatus,
+      body: { message, error: true }
+    }
+  }
+}
---
+++ notes.md
@@ -0,0 +1,4 @@
+List users
+```
+az rest -u "https://management.azure.com/subscriptions/52512f28-c6ed-403e-9569-82a9fb9fec91/resourcegroups/Live.Taskr/providers/Microsoft.Web/staticSites/taskr/authproviders/all/listUsers?api-version=2019-12-01-preview" --method post
+```
